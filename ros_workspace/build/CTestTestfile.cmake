73991d7df4a85c26e2e3c053d40c50e230b529a1
yashas <yashas.salankimatt@gmail.com>
2021-11-02 21:14:12 -0500

    init commit, with apartment model imported and gmapping launch script changed

---
+++ ros_workspace/build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/yashas/Documents/URS/ros_workspace/src
+# Build directory: /home/yashas/Documents/URS/ros_workspace/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("rosbot_description/src/rosbot_navigation")
+subdirs("rosbot_description/src/rosbot_description")
+subdirs("rosbot_description/src/rosbot_gazebo")
---
+++ ros_workspace/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/yashas/Documents/URS/ros_workspace/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/yashas/Documents/URS/ros_workspace/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/yashas/Documents/URS/ros_workspace/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD='/home/yashas/Documents/URS/ros_workspace/build'
+export PYTHONPATH="/home/yashas/Documents/URS/ros_workspace/devel/lib/python3/dist-packages:$PYTHONPATH"
+export ROS_PACKAGE_PATH="/home/yashas/Documents/URS/ros_workspace/src:$ROS_PACKAGE_PATH"
